4c50c0cbecba779535bf7e11a73e473f4402560e
Kehinde Ogunyele <kwitt.olawale@gmail.com>
2025-06-25 00:49:48 +0100

    Added: Smooth Scroll Behaviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { ScrollService } from './services/scroll.service';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,5 @@ import { SubscriptionFormComponent } from './subscription-form/subscription-form
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  private scrollService = inject(ScrollService)
 }
