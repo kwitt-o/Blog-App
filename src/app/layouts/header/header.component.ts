5d7db184a6885f81659c758af628f159439667ed
Kehinde Ogunyele <kwitt.olawale@gmail.com>
2025-06-24 23:39:37 +0100

    Update: Header, Footer, Logo, Mobile Menu

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CategoryNavbarComponent } from '../category-navbar/category-navbar.component';
+import { CommonModule } from '@angular/common';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  categories: any[] = [];
+  isMenuOpen = false;
+  isMobileDropdownOpen = false;
+
+  private categoryService = inject(CategoriesService);
+
+  ngOnInit(): void {
+    this.categoryService.loadData().subscribe(data => {
+      this.categories = data;
+      // console.log(data);
+    });
+
+
+  }
+
+
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+     this.isMobileDropdownOpen = false;
+  }
+
+  toggleMobileDropdown() {
+  this.isMobileDropdownOpen = !this.isMobileDropdownOpen;
+}
+
 
 }
